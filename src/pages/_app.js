892282410fc33d0c41e4b6acba29033880466f7b
Poleac Liviu Stefan <stefan.liviu28^2yahoo.ro>
2024-01-23 17:37:17 +0200

    backToLoading

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -25,6 +25,14 @@ export default function App({ Component, pageProps }) {
     setAgeConfirmed(true);
   };
 
+  if (ageConfirmed === null) {
+    return <div>Loading...</div>;
+  }
+
+  if (!ageConfirmed) {
+    return <CheckAge checkAgeToParent={handleAgeConfirmation} />
+  }
+
   return (
   <>
   <UserContextProvider>
