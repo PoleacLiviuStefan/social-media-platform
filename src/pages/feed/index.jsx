3d223b6d27bc5eadec8b7bd5b825df956db55be9
Poleac Liviu Stefan <stefan.liviu28^2yahoo.ro>
2024-01-27 21:41:48 +0200

    checked

--- src/pages/feed/index.jsx
+++ src/pages/feed/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import axios from "axios";
 import Media from "../../components/Media/Media"; // Adjust the import path as needed
 import { useRouter } from 'next/router';
