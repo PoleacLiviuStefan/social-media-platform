d507e43e5dc4b7620d115c55d671479692ee14df
Poleac Liviu Stefan <stefan.liviu286@gmail.com>
2024-03-11 23:40:55 +0200

    updatedBackendDomain

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import Head from 'next/head';
-import Image from 'next/image';
+
 import { Inter } from '@next/font/google';
-import styles from '@/styles/Home.module.css';
-import CheckAge from '../components/CheckAge/CheckAge';
-import Cookies from 'js-cookie';
+
 
 const inter = Inter({ subsets: ['latin'] });
 
